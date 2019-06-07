815334364320f705bdbc32216d9836d21a51d547
Prashant <bhandariprashant616@gmail.com>
2019-06-06 20:34:55 -0500

    Initial ChckIn

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+app.use(morgan('dev'));
+app.get('/sum', (req, res) => {
+    let firstNum = req.query.a;
+    let secondNum = req.query.b;
+    let result = Number(firstNum) + Number(secondNum);
+    console.log(`Sum is ${result}`);
+    res.send(`The Sum of ${firstNum} and ${secondNum} is ${result}`);
+  });
+
+  app.get('/cipher',(req,res)=>{
+let textVal = req.query.text;
+let shiftVal = Number(req.query.shift);
+let resultArr = [];
+for(let i=0;i<textVal.length;i++)
+{
+   let letterCode =  textVal.charCodeAt(i);
+   if((letterCode + shiftVal) > 90 )
+   {
+       let codeData = letterCode + shiftVal - 26;
+       resultArr.push(String.fromCharCode(codeData));
+
+   }
+   else
+   {
+       let codeData = letterCode + shiftVal;
+       resultArr.push(String.fromCharCode(codeData));
+   }
+}
+
+ res.send(`Result is ${resultArr.join('')}`);
+  });
+
+
+  app.get('/lotto',(req,res)=>{
+    let winnNumArr = [];
+    let numbers = req.query.arr;
+    let nbrCorrect = 0;
+    //generate 6 random numbers between 1 and 20
+    for(let i = 0 ; i<6;i++)
+    {
+        let randNum =  Math.floor((Math.random()*20) +1);
+        winnNumArr.push(randNum);
+    }
+    for(let i = 0; i<6; i++)
+    {
+        let numbrCheck = numbers[i];
+        let winnNumLength = winnNumArr.filter(n=>n===numbrCheck);
+        if(winnNumLength.length > 0)
+        {
+            nbrCorrect++;
+        }
+        console.log(`Number Check ${numbrCheck}`);
+
+    }
+   
+    res.send(`Hit the lotto url ! `);  
+  });
+
+  app.listen(8000, () => {
+    console.log('Express server is listening on port 8000!');
+  });
